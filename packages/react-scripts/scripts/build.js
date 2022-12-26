8e71fcb63c0582d9175526ea9528f740cb12d883
Bastien Gandouet <bastien@ouihelp.fr>
2022-12-26 11:38:08 +0100

    Scope to @ouihelp-npm

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -32,7 +32,7 @@ const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
 const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
-const FileSizeReporter = require('@ouihelp/react-dev-utils/FileSizeReporter');
+const FileSizeReporter = require('@ouihelp-npm/react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
 
 const measureFileSizesBeforeBuild =
